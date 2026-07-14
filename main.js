8317706ad84391e43742c3913239090d58cf9843
Mohamed <asd2114252@gmail.com>
2026-07-14 22:46:14 +0300

    First Version

---
+++ main.js
@@ -0,0 +1,26 @@
+// Variables
+let userInput = document.getElementsByName("user")[0],
+    reposBtn = document.querySelector("input[type=button]"),
+    theData = document.querySelector(".repos"); 
+
+
+reposBtn.onclick = getReposFun;
+
+function getReposFun (){
+    if(userInput.value){
+        theData.innerHTML = "";
+        fetch(`https://api.github.com/users/${userInput.value}/repos`)
+        .then(res=>res.json()).then(reps=>reps.forEach(rep=>{
+            let myDiv  = document.createElement("div"),
+                name = `<span>${rep.name}</span>`,
+                stars = `<span class = "stars">Stars : ${rep.stargazers_count}</span>`,
+                date = `<span>${Date(rep.created_at)}</span>`,
+                location = `<a href="https://github.com/${userInput.value}/${rep.name}" target= "_blank">Visit</a>`;
+                myDiv.innerHTML = name + date + location + stars;
+                myDiv.classList.add("repo-box")
+                theData.appendChild(myDiv)
+            }))
+    }else{
+        theData.innerHTML = "<h1 class = `empty`>Write The User Name</h1>"
+    }
+};
