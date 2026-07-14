24f353569e949ad8b670b759373c5268b626de41
Mohamed <asd2114252@gmail.com>
2026-07-14 23:32:51 +0300

    new

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function getReposFun (){
             let myDiv  = document.createElement("div"),
                 name = `<span>${rep.name}</span>`,
                 stars = `<span class = "stars">Stars : ${rep.stargazers_count}</span>`,
-                date = `<span>${Date(rep.created_at)}</span>`,
+                date = `<span class = "date">Created At :${new Date(rep.created_at).toDateString()}</span>`,
                 location = `<a href="https://github.com/${userInput.value}/${rep.name}" target= "_blank">Visit</a>`;
                 myDiv.innerHTML = name + date + location + stars;
                 myDiv.classList.add("repo-box")
